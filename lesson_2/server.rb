ed803c83db6e4cc86c4a69d618e96f9b39cb162d
Alyssa Easter <AlyssaStarley@gmail.com>
2025-01-24 16:47:08 -0800

    Completed Parsing Path and Parameters assignment

--- lesson_2/server.rb
+++ lesson_2/server.rb
@@ -1,4 +1,15 @@
 require "socket"
+def parse_request(request_line)
+  http_method, path_and_params, http = request_line.split
+  path, params = path_and_params.split("?")
+
+  params = params.split("&").each_with_object({}) do |param, hsh|
+    key, value = param.split("=")
+    hsh[key] = value
+  end
+
+  [http_method, path, params]
+end
 
 server = TCPServer.new("localhost", 3003)
 loop do
@@ -8,8 +19,19 @@ loop do
   next if !request_line || request_line =~ /favicon/
   puts request_line
 
+  http_method, path, params = parse_request(request_line)
+
   client.puts "HTTP/1.1 200 OK"
   client.puts "Content-Type: text/plain\r\n\r\n"
+  
   client.puts request_line
+  client.puts http_method
+  client.puts path
+  client.puts params
+
+  params["rolls"].to_i.times do
+    client.puts rand(params["sides"].to_i) + 1
+  end
+
   client.close
 end
